a731fbadb46280d471e0c3f50b17f5982ca005b4
Ravan Gozalov <ravangozalov@Ravans-Air.lan>
2023-11-29 12:26:44 +0400

    whole edit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,79 @@
+import React, { useContext, useEffect, useState } from "react";
+import headerLogo from "../assets/images/header-logo.png";
+import CartNumber from "./CartNumber";
+import FavoriteNumber from "./FavoriteNumber";
+import SearchInput from "./SearchInput";
+import Select from "./Select";
+import { Link } from "react-router-dom";
+import { CartContext } from "../cartContext";
+
+const Navbar = () => {
+  const { isUser, checkUser, logOut } = useContext(CartContext);
+
+  useEffect(() => {
+    checkUser();
+  }, []);
+
+  const [clicked, setClicked] = useState(false);
+
+  const handleClick = () => {
+    setClicked(!clicked);
+  };
+
+  return (
+    <>
+      <div className="navBar">
+        <div className="contain">
+          <Link to="/" className="navbar1">
+            <img src={headerLogo} alt="header-logo" />
+          </Link>
+          <div className="navbar2">
+            <div className="navbarCenter">
+              <SearchInput />
+            </div>
+            <div className="navbarRight">
+              <Select />
+              <Link to={"/favorites"}>
+                <FavoriteNumber />
+              </Link>
+              <Link to={"/cart"}>
+                <CartNumber />
+              </Link>
+
+              <UserDrop />
+              {isUser ? (
+                <button className="logOut" onClick={logOut}>
+                  <FiLogOut fontSize="1.4rem" color="#10071880" />
+                </button>
+              ) : null}
+            </div>
+          </div>
+          <div className="navbarElse">
+            <i className="fa-solid searchIcon fa-xl fa-magnifying-glass" onClick={handleClick}></i>
+            <i className="fa-solid locationIcon fa-xl fa-location-arrow"></i>
+          </div>
+        </div>
+      </div>
+      {clicked && (
+        <div>
+          <form>
+            <input type="text" className="responsiveSearch" placeholder="Search" />
+            {/* <SearchInput/> */}
+          </form>
+        </div>
+      )}
+      
+      <div className="navbarBottom"> 
+        <div className="container">
+          <div className="navbarbottomRight">
+            <TbLocation className="locationIcon" fontSize="1.7rem" />
+            <span>Follow the order</span>
+          </div>
+        </div>
+      </div>
+      <Sidebar />
+    </>
+  );
+};
+
+export default Navbar;
---
+++ src/components/SwiperComponent.jsx
@@ -0,0 +1,153 @@
+import { Navigation, Pagination, Scrollbar } from "swiper";
+import "swiper/swiper.min.css";
+import swiperImage1 from "";//meat
+import swiperImage2 from "";//fruits
+import swiperImage3 from "";//vegetables
+import swiperImage4 from "";//grains
+import swiperImage5 from "";//herbs
+import swiperImage6 from "";//spices
+import swiperImage7 from "";//diary products
+import swiperImage8 from "";//oils
+import swiperImage9 from "";//drinks
+
+
+
+
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Link } from "react-router-dom";
+
+const SwiperComponent = () => {
+  return (
+    <div className="swiper-cards-wrapper">
+      <Swiper
+        breakpoints={{
+          320: {
+            slidesPerView: 1,
+          },
+          380: {
+            slidesPerView: 2,
+          },
+          700: {
+            slidesPerView: 2,
+          },
+          768: {
+            slidesPerView: 3,
+          },
+          920: {
+            slidesPerView: 3,
+          },
+          1100: {
+            slidesPerView: 4,
+          },
+        }}
+        loop={true}
+        speed={600}
+        autoplay={1000}
+        scrollbar={{ draggable: true }}
+        modules={[Navigation, Pagination, Scrollbar]}
+        spaceBetween={10}
+        slidesPerView={4}
+        navigation
+      >
+        <SwiperSlide>
+          <Link to={"/products"}>
+            <div className="swiper-slide-card">
+              <span className="swiper-slide-span">Meat</span>
+              <div className="swiper-slide-img-block">
+                <img src={swiperImage1} alt="" />
+              </div>
+            </div>
+          </Link>
+        </SwiperSlide>
+        <SwiperSlide>
+          <Link to={"/products"}>
+            <div className="swiper-slide-card">
+              <span className="swiper-slide-span">Fruits</span>
+              <div className="swiper-slide-img-block">
+                <img src={swiperImage2} alt="" />
+              </div>
+            </div>
+          </Link>
+        </SwiperSlide>
+        <SwiperSlide>
+          <Link to={"/products"}>
+            <div className="swiper-slide-card">
+              <span className="swiper-slide-span">Vegetables</span>
+              <div className="swiper-slide-img-block">
+                <img src={swiperImage3} alt="" />
+              </div>
+            </div>
+          </Link>
+        </SwiperSlide>
+        <SwiperSlide>
+          <Link to={"/products"}>
+            <div className="swiper-slide-card">
+              <span className="swiper-slide-span">Grains</span>
+              <div className="swiper-slide-img-block">
+                <img src={swiperImage4} alt="" />
+              </div>
+            </div>
+          </Link>
+        </SwiperSlide>
+        <SwiperSlide>
+          <Link to={"/products"}>
+            <div className="swiper-slide-card">
+              <span className="swiper-slide-span">Herbs</span>
+              <div className="swiper-slide-img-block">
+                <img src={swiperImage5} alt="" />
+              </div>
+            </div>
+          </Link>
+        </SwiperSlide>
+        <SwiperSlide>
+          <Link to={"/products"}>
+            <div className="swiper-slide-card">
+              <span className="swiper-slide-span">Spices</span>
+              <div className="swiper-slide-img-block">
+                <img src={swiperImage6} alt="" />
+              </div>
+            </div>
+          </Link>
+        </SwiperSlide>
+        <SwiperSlide>
+          <Link to={"/products"}>
+            <div className="swiper-slide-card">
+              <span className="swiper-slide-span">Diary Products</span>
+              <div className="swiper-slide-img-block">
+                <img src={swiperImage7} alt="" />
+              </div>
+            </div>
+          </Link>
+        </SwiperSlide>
+        <SwiperSlide>
+          <Link to={"/products"}>
+            <div className="swiper-slide-card">
+              <span className="swiper-slide-span">Oils</span>
+              <div className="swiper-slide-img-block">
+                <img src={swiperImage8} alt="" />
+              </div>
+            </div>
+          </Link>
+        </SwiperSlide>
+        <SwiperSlide>
+          <Link to={"/products"}>
+            <div className="swiper-slide-card">
+              <span className="swiper-slide-span">Drinks</span>
+              <div className="swiper-slide-img-block">
+                <img src={swiperImage9} alt="" />
+              </div>
+            </div>
+          </Link>
+        </SwiperSlide>
+
+
+        
+      </Swiper>
+    </div>
+  );
+};
+
+export default SwiperComponent;
