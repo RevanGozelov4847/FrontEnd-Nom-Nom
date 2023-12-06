772c4ef09dd94742d8511bf53fba0c343a3244cc
Ravan Gozalov <ravangozalov@Ravans-Air.lan>
2023-12-07 00:49:13 +0400

    “Messages”

--- src/App.js
+++ src/App.js
@@ -7,8 +7,27 @@ import Products from "./pages/Products";
 import Navbar from "./components/Navbar";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { AnimatePresence } from "framer-motion";
+import Loader from "./components/Loader";
+
+const [loader, setLoader] = useState(true);
+import { useLocation, useNavigate } from "react-router-dom";
+
+import { useEffect, useState } from "react";
+
 
 function App() {
+  const location = useLocation();
+  const navigate = useNavigate();
+  const [isUser, setIsUser] = useState(false);
+  const [loader, setLoader] = useState(true);
+
+  useEffect(() => {
+    checkUser();
+  }, []);
+  
+  setTimeout(() => {
+    setLoader(false);
+  }, 1000);
   return (
     <div className="App">
       <div>
@@ -19,6 +38,7 @@ function App() {
             <Routes>
               <Route path="/" element={<Home />} />
               <Route path="/login" element={<Login />} />
+              {isUser ? <Route path="/cart" element={<Cart />} /> : null}
               <Route path="/notFound" element={<NotFound />} />
               <Route path="/notFound" element={<EmptyCart />} />
               <Route path="/notFound" element={<Products />} />
