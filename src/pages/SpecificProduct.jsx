48cef8901a5c934305a7f69fd0e4cd092c666aee
Sama <mustafalinihad8@gmail.com>
2023-12-06 17:50:18 +0400

    scrolling

---
+++ src/pages/SpecificProduct.jsx
@@ -0,0 +1,7 @@
+import React, { useContext, useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { CartContext } from "../cartContext";
+import Accordion from "../components/Accordion";
+import ProductsSwiper from "../components/ProductsSwiper";
+import ResponsiveAccordion from "../components/ResponsiveAccordion";
+
