42b6f530cf3b6404e27dab531885b7ba2ca89180
Sama <mustafalinihad8@gmail.com>
2023-12-10 17:25:30 +0400

    dieticians

--- src/components/Professionals.jsx
+++ src/components/Professionals.jsx
@@ -10,26 +10,26 @@ const Professionals = () => {
   return (
     <section>
       <div className="containerMore">
-        <p className="title">Health Professionals</p>
+        <p className="title">Dieticians and Nutritionists</p>
         <p className="description-1">
-            You Can See all Professionals
+            You Can See all Dieticians and Nutritionists 
         </p>
         <p className="description-2">
-           Health Professionals with Their Quotes
+           Dieticians and Nutritionists with Their Quotes
         </p>
         <div className="profImages">
           <div className="prof-left">
             <Link to={"/products"}>
               <div className="prof-left-1">
                 <img className="prof1" src={prof1} alt="" />
-                <button className="button">Cookies</button>
+                <button className="button">Dr. Megan Rossi</button>
               </div>
             </Link>
 
             <Link to={"/products"}>
               <div className="prof-left-2">
                 <img className="prof2" src={prof2} alt="" />
-                <button className="button">Porfumery</button>
+                <button className="button">Tim Spector </button>
               </div>
             </Link>
           </div>
@@ -38,7 +38,7 @@ const Professionals = () => {
             <div className="prof-center">
               <div className="prof-center-1">
                 <img className="prof3" src={prof3} alt="" />
-                <button className="button">Flowers</button>
+                <button className="button">Christy Harrison</button>
               </div>
             </div>
           </Link>
@@ -47,14 +47,14 @@ const Professionals = () => {
             <Link to={"/products"}>
               <div className="prof-right-1">
                 <img className="prof4" src={prof4} alt="" />
-                <button className="button">Gifts</button>
+                <button className="button"> Alissa Rumsey </button>
               </div>
             </Link>
 
             <Link to={"/products"}>
               <div className="prof-right-2">
                 <img className="prof5" src={prof5} alt="" />
-                <button className="button">Cakes</button>
+                <button className="button">Rhiannon Lambert</button>
               </div>
             </Link>
           </div>
